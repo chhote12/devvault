f960dd4e82d5d389f933f6e839fedc7926a1843a
chhote12 <singhchhote695@gmail.com>
2025-07-08 17:00:31 +0530

    consloegogle

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,12 @@ import Navbar from "./components/Navbar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+          <meta 
+            name="google-site-verification" 
+            content="8FZvJ7XDE5e4SYoNe_Pcpx7jzI-UL-2lSt6jXXuOBi8" 
+          />
+        </head>
       <body >
         <Navbar></Navbar>
         {children}
