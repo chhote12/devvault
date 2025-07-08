56a06f2e52c21223a1ee02bd2f37afbdf8f6de95
chhote12 <singhchhote695@gmail.com>
2025-07-08 12:04:29 +0530

    remove tech

--- app/cpp/layout.jsx
+++ app/cpp/layout.jsx
@@ -1,7 +1,24 @@
 import Cpp from "./Cpp";
 import './layout.css'
 
+export const metadata = {
+  title: "Learn C++ Programming | CodePrune",
+  description: "Explore beginner to advanced C++ tutorials at CodePrune. Practice, build, and master C++ from scratch.",
+  keywords: "C++, C++ tutorials, learn C++, C++ examples, C++ for beginners, codeprune",
+  openGraph: {
+    title: "Learn C++ Programming | CodePrune",
+    description: "Master C++ with clear explanations, code samples, and real projects.",
+    url: "https://www.codeprune.com/cpp",
+    siteName: "CodePrune",
+    locale: "en_US",
+    type: "website",
+  },
+};
+
 export default function RoowtLayout({ children }) {
+
+
+  
   return (
     <div className="lay">
         <div className="l">
